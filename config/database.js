269e4f930a7d55c7de874a0a1218daa78f07ecf6
Hrishikesh Kalita <95520030+HrishiD89@users.noreply.github.com>
2023-06-11 23:42:02 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
   database:
     "mongodb+srv://<username>:<password>@cluster0.mzofbvq.mongodb.net/copyminiproject?retryWrites=true&w=majority",
-  secret: "yoursecret",
+  secret: "<secret>",
 };
